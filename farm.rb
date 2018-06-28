3de85361c907e91acf2aa2ab4f44362cfb79581f
slpatrycja <sl.patrycja.of@gmail.com>
2018-06-28 23:02:52 +0200

    ruby first break objects, classes, inheritance

---
+++ farm.rb
@@ -0,0 +1,50 @@
+class Animal
+
+	def give_sound
+	end
+end
+
+class Cat < Animal
+	def give_sound
+		puts 'Miau! Miau!'
+	end
+end
+
+class Dog < Animal
+	def give_sound
+		puts 'Woof! Woof!'
+	end
+end
+
+class Duck < Animal
+	def give_sound
+		puts 'Kwa! Kwa!'
+	end
+end
+
+class Goose < Animal
+	def give_sound
+		puts 'Gda! Gda!'
+	end
+end
+
+class Farm
+	def initialize(array)
+		@array = array
+	end
+
+	def give_sounds()
+		@array.each do |animal|
+			animal.give_sound
+		end
+	end
+end
+
+cat = Cat.new
+dog = Dog.new
+duck = Duck.new
+arr = []
+arr.push(cat,dog,duck)
+farm = Farm.new(arr)
+
+farm.give_sounds
